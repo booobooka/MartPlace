695782acbd7f53593ffe4fd83c4766658fca074e
booobooka <skriblle@ukr.net>
2020-04-07 17:50:14 +0300

    added featured products section

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+   
+});
+$('.products-slider-inner').slick({
+    arrows: true,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="..//images/icons/lnr-chevron-left.svg"></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="..//images/icons/lnr-chevron-right.svg"></button>',
+  });
