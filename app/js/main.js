eee8eac482ac1c262bbbb4fafd6e6f05c7e6daad
booobooka <skriblle@ukr.net>
2020-04-07 22:15:00 +0300

    added section release products

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,14 @@
 $(function(){
 
-   
+    var mixer = mixitup('.release-products-inner'); 
 });
+
 $('.products-slider-inner').slick({
     arrows: true,
     dots: false,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     prevArrow: '<button class="slick-arrow slick-prev"><img src="..//images/icons/lnr-chevron-left.svg"></button>',
     nextArrow: '<button class="slick-arrow slick-next"><img src="..//images/icons/lnr-chevron-right.svg"></button>',
   });
