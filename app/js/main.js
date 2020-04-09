c2a6cd1d01c18d2cf95e53897487877ab77ee5de
booobooka <skriblle@ukr.net>
2020-04-09 22:54:42 +0300

    added section featured products and slider

--- app/js/main.js
+++ app/js/main.js
@@ -3,12 +3,12 @@ $(function(){
     var mixer = mixitup('.release-products-inner'); 
 });
 
-$('.products-slider-inner').slick({
+$('.featured__products-slider__inner').slick({
     arrows: true,
     dots: false,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     prevArrow: '<button class="slick-arrow slick-prev"><img src="..//images/icons/lnr-chevron-left.svg"></button>',
     nextArrow: '<button class="slick-arrow slick-next"><img src="..//images/icons/lnr-chevron-right.svg"></button>',
   });
