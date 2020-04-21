e9c79cb89d8e5813c75f49ec0832963c05b19c7f
booobooka <skriblle@ukr.net>
2020-04-21 22:47:50 +0300

    added adaptive design for home page

--- app/js/main.js
+++ app/js/main.js
@@ -52,9 +52,34 @@ $('.followers-feed__slider__inner').slick({
   slidesToShow: 3,
   slidesToScroll: 3,
   autoplay: false,
-  variableWidth: true,
+  variableWidth: false,
   prevArrow: '<button class="slick-arrow slick-preve"><img src="..//images/icons/lnr-chevron-left.svg"></button>',
   nextArrow: '<button class="slick-arrow slick-nexte"><img src="..//images/icons/lnr-chevron-right.svg"></button>',
+  responsive: [
+    {
+      breakpoint: 1190,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 760,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  
+  ]
 });
 
 $('.menu__btn').on('click', function(){
@@ -64,3 +89,4 @@ $('.menu__btn').on('click', function(){
 $('.header__btn-menu').on('click', function(){
   $('.header__icons').toggleClass('active');
 });
+
